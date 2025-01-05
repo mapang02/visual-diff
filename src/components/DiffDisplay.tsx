fc6ef60b07153d8dad4adee6990c6a7346317313
mapang02 <marcuspang02@gmail.com>
2025-01-05 01:45:20 -0500

    Testing a diff format that prioritizes lines first

--- src/components/DiffDisplay.tsx
+++ src/components/DiffDisplay.tsx
@@ -1,5 +1,5 @@
 import { JSX } from "preact";
-import { diffWords, diffLines, diffWordsWithSpace, diffArrays, Change, createPatch, structuredPatch } from "diff";
+import { diffWords, diffLines, diffWordsWithSpace, diffArrays, Change, createPatch, structuredPatch, diffChars } from "diff";
 
 interface DiffDisplayProps {
     origText: string;
@@ -18,236 +18,169 @@ const REGULAR_TEXT_STYLE = "";
 const REMOVED_LINE_STYLE = "table-cell text-left bg-red-200/50";
 const ADDED_LINE_STYLE = "table-cell text-left bg-green-200/50";
 const REGULAR_LINE_STYLE = "table-cell text-left";
-const FILLER_LINE_STYLE = "table-cell text-left bg-gray-50/10"
+const FILLER_LINE_STYLE = "table-cell text-left bg-gray-50/10";
+const LINE_NUM_STYLE = "table-cell text-right pr-2";
 
-function formatDiffText(origText: string, changedText: string) {
-    const diff = diffWordsWithSpace(origText, changedText);
-    console.log(diff);
-
-    let [oLineSpans, cLineSpans] = arrangeIntoLines(diff);
-    console.log(oLineSpans);
-    console.log(cLineSpans);
-
-    // Make word diff prettier by merging diffs separated by non-edited spaces
-    const allWhitespace = /^\s+$/;
-    oLineSpans.forEach((oLine) => {
-        for (let i = 1; i < oLine.length - 1; i++) {
-            if (!!oLine[i - 1].removed && !!oLine[i + 1].removed && !oLine[i].removed && oLine[i].text.match(allWhitespace)) {
-                oLine[i].removed = true;
-            }
-        }
-    });
-    cLineSpans.forEach((cLine) => {
-        for (let i = 1; i < cLine.length - 1; i++) {
-            if (!!cLine[i - 1].added && !!cLine[i + 1].added && !cLine[i].added && cLine[i].text.match(allWhitespace)) {
-                cLine[i].added = true;
-            }
-        }
-    });
+function formatDiffLines(oldText: string, newText: string) {
+    const lineDiff = diffLines(oldText, newText, { newlineIsToken: false, ignoreNewlineAtEof: false });
+    console.log(lineDiff);
     
+    // Separate into old lines and new lines, add padding lines so that unedited lines stay at the same level
+    let oLines: Change[] = [];
+    let nLines: Change[] = [];
+    const paddingLine: Change = { count: 0, value: "", added: false, removed: false};
+    lineDiff.forEach((ch, lineno) => {
+        const lines = ch.value.split("\n").slice(0, (lineno === lineDiff.length - 1) ? undefined : -1);
+        if (ch.removed) {
+            lines.forEach((ln) => {
+                oLines.push({ count: 1, value: ln, added: ch.added, removed: ch.removed });
+            });
+        }
+        else if (ch.added) {
+            lines.forEach((ln) => {
+                nLines.push({ count: 1, value: ln, added: ch.added, removed: ch.removed });
+            });
+        }
+        else {
+            while (nLines.length > oLines.length) {
+                oLines.push(paddingLine);
+            }
+            while (oLines.length > nLines.length) {
+                nLines.push(paddingLine);
+            }
+            lines.forEach((ln) => {
+                oLines.push({ count: 1, value: ln, added: ch.added, removed: ch.removed });
+                nLines.push({ count: 1, value: ln, added: ch.added, removed: ch.removed });
+            });
+        }
+    });
+    // Add final padding lines if needed
+    console.log(`oLines.length: ${oLines.length}, nLines.length: ${nLines.length}`);
+    while (nLines.length > oLines.length) {
+        oLines.push(paddingLine);
+    }
+    while (oLines.length > nLines.length) {
+        nLines.push(paddingLine);
+    }
+
+    console.log("oLines:");
+    console.log(oLines);
+    console.log("nLines:");
+    console.log(nLines);
+
+    // Compare each line, and convert to HTML line by line
     let visualDiffLines: JSX.Element[] = []
-    let oNumLines = 0;
-    let cNumLines = 0;
-    for (let i = 0; i < oLineSpans.length; i++) {
-        // Convert TextSpan objects to HTML spans
-        let hasRemovedText = false;
-        const oParsedSpans = oLineSpans[i].map((textSpan) => {
-            if (textSpan.removed) {
-                hasRemovedText = true;
-                return (
-                    <span class={REMOVED_TEXT_STYLE}>{textSpan.text}</span>
+    let oLineCount = 0;
+    let nLineCount = 0;
+    for (let i = 0; i < oLines.length; i++) {
+        let oLineText = <div class={FILLER_LINE_STYLE}></div>
+        let nLineText = <div class={FILLER_LINE_STYLE}></div>
+
+        if (oLines[i].count === 0) {
+            nLineText = (
+                <div class={ADDED_LINE_STYLE}>
+                    <span class={ADDED_TEXT_STYLE}>
+                        {nLines[i].value}
+                    </span>
+                </div>
+            );
+            nLineCount += 1;
+        }
+        else if (nLines[i].count === 0) {
+            oLineText = (
+                <div class={REMOVED_LINE_STYLE}>
+                    <span class={REMOVED_TEXT_STYLE}>
+                        {oLines[i].value}
+                    </span>
+                </div>
+            );
+            oLineCount += 1;
+        }
+        else { // Determine diff between matched lines
+            if (!oLines[i].removed && !nLines[i].added) {
+                oLineText = (
+                    <div class={REGULAR_LINE_STYLE}>
+                        <span>
+                            {oLines[i].value}
+                        </span>
+                    </div>
+                );
+                nLineText = (
+                    <div class={REGULAR_LINE_STYLE}>
+                        <span>
+                            {nLines[i].value}
+                        </span>
+                    </div>
                 );
             }
-            return (
-                <span class={REGULAR_TEXT_STYLE}>{textSpan.text}</span>
-            );
-        });
-        const oLineStyle = hasRemovedText ? REMOVED_LINE_STYLE : (oParsedSpans.length === 0 ? FILLER_LINE_STYLE : REGULAR_LINE_STYLE);
-        if (oParsedSpans.length > 0) {
-            oNumLines += 1;
-        }
-        const oLineCount = <div class="table-cell text-right">{oParsedSpans.length > 0 ? oNumLines : ""}</div>;
-        const oLineText = <div class={oLineStyle}>{oParsedSpans}</div>;
+            else {
+                let diff = diffWordsWithSpace(oLines[i].value, nLines[i].value);
+                let oLineSpans: JSX.Element[] = [];
+                let nLineSpans: JSX.Element[] = [];
+                diff.forEach((ch) => {
+                    if (ch.removed) {
+                        const removedSpan = <span class={REMOVED_TEXT_STYLE}>{ch.value}</span>;
+                        oLineSpans.push(removedSpan);
+                    }
+                    else if (ch.added) {
+                        const addedSpan = <span class={ADDED_TEXT_STYLE}>{ch.value}</span>;
+                        nLineSpans.push(addedSpan);
+                    }
+                    else {
+                        const sharedSpan = <span class={REGULAR_TEXT_STYLE}>{ch.value}</span>;
+                        oLineSpans.push(sharedSpan);
+                        nLineSpans.push(sharedSpan);
+                    }
+                });
 
-        let hasAddedText = false;
-        const cParsedSpans = cLineSpans[i].map((textSpan) => {
-            if (textSpan.added) {
-                hasAddedText = true;
-                return (
-                    <span class={ADDED_TEXT_STYLE}>{textSpan.text}</span>
+                oLineText = (
+                    <div class={REMOVED_LINE_STYLE}>
+                        <span>
+                            {oLineSpans}
+                        </span>
+                    </div>
+                );
+                nLineText = (
+                    <div class={ADDED_LINE_STYLE}>
+                        <span>
+                            {nLineSpans}
+                        </span>
+                    </div>
                 );
             }
-            return (
-                <span class={REGULAR_TEXT_STYLE}>{textSpan.text}</span>
-            );
-        });
-        const cLineStyle = hasAddedText ? ADDED_LINE_STYLE : (cParsedSpans.length === 0 ? FILLER_LINE_STYLE : REGULAR_LINE_STYLE);
-        if (cParsedSpans.length > 0) {
-            cNumLines += 1;
+            oLineCount += 1;
+            nLineCount += 1;
         }
-        const cLineCount = <div class="table-cell text-right">{cParsedSpans.length > 0 ? cNumLines : ""}</div>;
-        const cLineText = <div class={cLineStyle}>{cParsedSpans}</div>;
 
+        const oLineNumDisp = <div class={LINE_NUM_STYLE}>{oLines[i].count ? oLineCount : ""}</div>
+        const nLineNumDisp = <div class={LINE_NUM_STYLE}>{nLines[i].count ? nLineCount : ""}</div>
         const visualDiffRow = (
             <div class="table-row">
-                {oLineCount}
+                {oLineNumDisp}
                 {oLineText}
-                {cLineCount}
-                {cLineText}
+                {nLineNumDisp}
+                {nLineText}
             </div>
         );
         visualDiffLines.push(visualDiffRow);
     }
 
+    return visualDiffLines;
+}
+
+export default function DiffDisplay(props: DiffDisplayProps) {
+    const visualDiffLines = formatDiffLines(props.origText, props.changedText);
     return (
         <table class="table-fixed w-full">
             <thead>
                 <tr>
-                    <th class="w-10"/>
+                    <th class="w-8"/>
                     <th/>
-                    <th class="w-10"/>
+                    <th class="w-8"/>
                     <th/>
                 </tr>
             </thead>
             {visualDiffLines}
         </table>
     );
-
-    // Format spans into line divs
-    /*
-    const oLineDivs = oLineSpans.map((oLine, i) => {
-        // Convert TextSpan objects to HTML spans
-        let hasRemovedText = false;
-        const parsedSpans = oLine.map((textSpan) => {
-            if (textSpan.removed) {
-                hasRemovedText = true;
-                return (
-                    <span class={REMOVED_TEXT_STYLE}>{textSpan.text}</span>
-                );
-            }
-            return (
-                <span class={REGULAR_TEXT_STYLE}>{textSpan.text}</span>
-            );
-        });
-
-        // Build the div for the line
-        const lineStyle = hasRemovedText ? REMOVED_LINE_STYLE : (parsedSpans.length === 0 ? FILLER_LINE_STYLE : REGULAR_LINE_STYLE);
-        return (
-            <div class="table-row">
-                <div class="table-cell text-right">{i + 1}</div>
-                <div class={lineStyle}>{parsedSpans}</div>
-            </div>
-        )
-    });
-    const cLineDivs = cLineSpans.map((cLine, i) => {
-        // Convert TextSpan objects to HTML spans
-        let hasAddedText = false;
-        const parsedSpans = cLine.map((textSpan) => {
-            if (textSpan.added) {
-                hasAddedText = true;
-                return (
-                    <span class={ADDED_TEXT_STYLE}>{textSpan.text}</span>
-                );
-            }
-            return (
-                <span class={REGULAR_TEXT_STYLE}>{textSpan.text}</span>
-            );
-        });
-
-        // Build the div for the line
-        return (
-            <div class="table-row">
-                <div class="table-cell text-right">{i + 1}</div>
-                <div class={hasAddedText ? ADDED_LINE_STYLE : REGULAR_LINE_STYLE}>{parsedSpans}</div>
-            </div>
-        )
-    });
-
-    const oLineSection = (
-        <div class="table w-full">
-            <div class="table-row">
-                {oLineDivs}
-            </div>
-        </div>
-    );
-    const cLineSection = (
-        <div class="table w-full">
-            <div class="table-row">
-                {cLineDivs}
-            </div>
-        </div>
-    );
-    return [oLineSection, cLineSection];
-    */
-}
-
-function arrangeIntoLines(changes: Change[]) {
-    let oLineSpans: TextSpan[][] = [];
-    let cLineSpans: TextSpan[][] = [];
-    let oCurrLine: TextSpan[] = [];
-    let cCurrLine: TextSpan[] = [];
-    changes.forEach((ch) => {
-        // Start adding text to new line if \n is encountered in diff
-        ch.value.split(/(\n)/).filter(s => s.length > 0).forEach((span) => {
-            if (span === "\n") {
-                // If this is within a change, add a dummy span so that empty line is colored correctly when processing later
-                if (ch.removed) {
-                    if (oCurrLine.length == 0) {
-                        oCurrLine.push({text: "", removed: true});
-                    }
-                }
-                else if (ch.added) {
-                    if (cCurrLine.length == 0) {
-                        cCurrLine.push({text: "", added: true});
-                    }
-                }
-
-                // Start new line
-                if (!ch.removed) {
-                    cLineSpans.push(cCurrLine);
-                    cCurrLine = [];
-                }
-                if (!ch.added) {
-                    oLineSpans.push(oCurrLine);
-                    oCurrLine = [];
-                }
-            }
-            else {
-                // Pad the smaller array so line numbers for shared chunks match
-                if (!ch.added && !ch.removed) {
-                    while (oLineSpans.length < cLineSpans.length) {
-                        oLineSpans.push([]);
-                    }
-                    while (cLineSpans.length < oLineSpans.length) {
-                        cLineSpans.push([]);
-                    }
-                }
-
-                if (!ch.added) {
-                    oCurrLine.push({text: span, removed: ch.removed});
-                }
-                if (!ch.removed) {
-                    cCurrLine.push({text: span, added: ch.added});
-                }
-            }
-        });
-    });
-    oLineSpans.push(oCurrLine);
-    cLineSpans.push(cCurrLine);
-    return [oLineSpans, cLineSpans];
-}
-
-export default function DiffDisplay(props: DiffDisplayProps) {
-    /*
-    const [oText, cText] = formatDiffText(props.origText, props.changedText);
-    return (
-        <>
-            <div class="text-left grid grid-cols-2 whitespace-pre">
-                <div>{oText}</div>
-                <div>{cText}</div>
-            </div>
-        </>
-    );
-    */
-   return formatDiffText(props.origText, props.changedText);
 }
